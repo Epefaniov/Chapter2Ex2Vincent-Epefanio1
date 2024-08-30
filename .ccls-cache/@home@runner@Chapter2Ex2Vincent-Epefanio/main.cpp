ad7d13bce59bbedc565f929f6646cff616dfa07a
Vincent Epefanio <vepefanio@gmail.com>
2024-08-30 14:25:58 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Chapter2Ex2Vincent-Epefanio/main.cpp
@@ -0,0 +1,39 @@
+/*
+Title: Chapter 2 Excerise 2 - Do something 
+File Name: Chapter2Ex2_Mark_Goldstein.cpp
+Programmer: Vincent Epefanio
+Date: 08/2024
+Requirements:
+The East coast sales division of a company generates 58 percent of total sales. Based on that percentage, write a program that will predict how much the East Coast division will generate if the company has $8.6 million in sales this year.
+*/
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() 
+
+{ 
+  
+  
+  int number1;
+  int number2; 
+  int answer;
+  cout<<"Please end a number\n";
+  cin>>number1;
+  cout<<"Please enter a second number\n";
+  cin>> number2;
+  answer = number1 + number2;
+  cout<< "the sum of" << number1 << "and" << number2 << "is" << answer;
+  
+  
+  
+return 0;// this is to return the value defined by the main function
+  
+}
+
